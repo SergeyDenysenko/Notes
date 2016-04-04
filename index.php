c4d589a187cd5a8a056beed5ad655bfa000a080f
SergeyDenysenko <sergeystarscream@gmail.com>
2016-04-04 16:19:14 +0200

    Connection to db

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-	echo 'usigusg';
-	echo 'dsgsd';
-	cfgygy;
+	$dblocation = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbLink = @mysql_connect($dblocation, $dbuser, $dbpass);
+    if(!$dbLink) echo 'Connecting problem';
+    /* else 
+    {
+        if(mysql_query($sql)) echo 'Table has alredy created';
+        else {
+            mysql_select_db('Notes'); 
+            $sql = "CREATE TABLE  `users` (`id` INT NOT NULL ,`user` VARCHAR( 100 ) CHARACTER 
+            SET utf8 COLLATE utf8_general_ci NOT NULL , PRIMARY KEY (  `id` ))";   
+            if(mysql_query($sql)) echo 'Table is created';
+            else echo 'Table is not created';
+        }
+    } */
 ?>
